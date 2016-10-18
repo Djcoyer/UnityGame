4712641e5a3254c6f51ba3368447d1840e1c51cf
Devyn Coyer <devyn@coyer.net>
2016-10-18 08:38:16 -0500

    Intitial check in

---
+++ CSEGameProject/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class SpawnScript : MonoBehaviour {
+
+    public GameObject PickUp;
+    private Vector3 position;
+    private Quaternion quaternian = new Quaternion(0,0,0,0);
+    private Stopwatch sw = new Stopwatch();
+    // Use this for initialization
+    void Start()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        sw = new Stopwatch();
+        sw.Start();
+        if(sw.Elapsed.Seconds % 2 == 0)
+        {
+            if (sw.Elapsed.Seconds <= 60)
+            {
+                float xRange = Random.Range(-9, 9);
+                float zRange = Random.Range(-9, 9);
+                spawnCube(xRange, zRange, sw);
+
+            }
+        }
+        
+
+    }
+
+    private void spawnCube(float x, float z, Stopwatch sw)
+    {
+            Object.Instantiate(PickUp, new Vector3(x, 0.5f, z), quaternian);
+        
+        
+    }
+}
